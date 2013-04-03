97769a3ec4e4d1c888b2c703d3e03b9e9ceda9d7
mo khan <mo@mokhan.ca>
2013-04-02 21:10:07 -0600

    correct the key for each category

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -10,20 +10,28 @@ class AppDelegate
         {
           title: "Review",
           rows: [
-            { title: "Broth", key: :slider, type: :slider, range: (1..10), value: 7 },
-            { title: "Beef", key: :slider, type: :slider, range: (1..10), value: 7 },
-            { title: "Noodles", key: :slider, type: :slider, range: (1..10), value: 7 },
-            { title: "Vegetables", key: :slider, type: :slider, range: (1..10), value: 7 },
-            { title: "Bowl Size", key: :slider, type: :slider, range: (1..10), value: 7 },
-            { title: "Sauces", key: :slider, type: :slider, range: (1..10), value: 7 },
-            { title: "Presentation", key: :slider, type: :slider, range: (1..10), value: 7 },
-            { title: "Ambiance", key: :slider, type: :slider, range: (1..10), value: 7 },
+            { title: "Broth", key: :broth, type: :slider, range: (1..10), value: 7 },
+            { title: "Beef", key: :beef, type: :slider, range: (1..10), value: 7 },
+            { title: "Noodles", key: :noodles, type: :slider, range: (1..10), value: 7 },
+            { title: "Vegetables", key: :vegetables, type: :slider, range: (1..10), value: 7 },
+            { title: "Bowl Size", key: :bowl, type: :slider, range: (1..10), value: 7 },
+            { title: "Sauces", key: :sauces, type: :slider, range: (1..10), value: 7 },
+            { title: "Presentation", key: :presentation, type: :slider, range: (1..10), value: 7 },
+            { title: "Ambiance", key: :ambiance, type: :slider, range: (1..10), value: 7 },
           ]
         },
         {
           rows: [ { title: "Submit", type: :submit, }]
         }]
     })
+    @form.on_submit do |form|
+      BW::Location.get do |result|
+        BW::Location.stop
+        p form.render
+        #location = Restaurant.new(@textbox.text, result[:to].latitude, result[:to].longitude)
+        #location.save
+      end
+    end
 
     @form_controller = Formotion::FormController.alloc.initWithForm(@form)
     @window.rootViewController = @form_controller
