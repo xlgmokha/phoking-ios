73bc2a66125a626dfd2296333bc3fc91f989dd69
mo khan <mo@mokhan.ca>
2013-04-02 21:00:42 -0600

    try formotion for rendering a quick review form

--- Gemfile
+++ Gemfile
@@ -1,2 +1,3 @@
 source "https://rubygems.org"
 gem 'bubble-wrap'
+gem 'formotion'
--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,9 +1,34 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    controller_factory = ControllerFactory.new
-    @window.rootViewController = UINavigationController.alloc.initWithRootViewController(controller_factory.create(SearchController))
+    #controller_factory = ControllerFactory.new
+    #@window.rootViewController = UINavigationController.alloc.initWithRootViewController(controller_factory.create(SearchController))
+    #@window.makeKeyAndVisible
+
+    @form = Formotion::Form.new({
+      sections: [
+        {
+          title: "Review",
+          rows: [
+            { title: "Broth", key: :slider, type: :slider, range: (1..10), value: 7 },
+            { title: "Beef", key: :slider, type: :slider, range: (1..10), value: 7 },
+            { title: "Noodles", key: :slider, type: :slider, range: (1..10), value: 7 },
+            { title: "Vegetables", key: :slider, type: :slider, range: (1..10), value: 7 },
+            { title: "Bowl Size", key: :slider, type: :slider, range: (1..10), value: 7 },
+            { title: "Sauces", key: :slider, type: :slider, range: (1..10), value: 7 },
+            { title: "Presentation", key: :slider, type: :slider, range: (1..10), value: 7 },
+            { title: "Ambiance", key: :slider, type: :slider, range: (1..10), value: 7 },
+          ]
+        },
+        {
+          rows: [ { title: "Submit", type: :submit, }]
+        }]
+    })
+
+    @form_controller = Formotion::FormController.alloc.initWithForm(@form)
+    @window.rootViewController = @form_controller
     @window.makeKeyAndVisible
+
     true
   end
 end
